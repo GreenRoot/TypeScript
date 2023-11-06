941d9ad25c84e355e83377bbaf2721dd54138f19
GreenRoot <1.ooo.ooo@bk.ru>
2023-11-06 13:23:07 +0300

    learn types

--- app.ts
+++ app.ts
@@ -1,1 +1,7 @@
-const a = 1;
+function getFullName(firstname: string, surname: string): string {
+    return `${firstname} ${surname}`
+}
+
+const getFullNameArrow = (firstname: string, surname: string): string => `${firstname} ${surname}`
+
+console.log('Биба', 'Боба');
