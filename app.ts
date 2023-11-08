eb15afcd8c1128d364cc3d3ebbbfef4202faffd8
GreenRoot <1.ooo.ooo@bk.ru>
2023-11-08 19:58:29 +0300

    learn enums

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
-const skills: readonly [string, string, string, number] = ['dev', 'front', 'back', 1];
-// skills[0] = 'design' no valid
-
-const [dev, front, back, id] = skills;
-
-const arr: [number, string, ...boolean[]] = [1, 'str', true, false]
+enum StatusCode {
+    SUCCESS ,
+    IN_PROGRESS ,
+    FAILED
+}
+const res= {
+    message: 'Платёж успешен',
+    statusCode: StatusCode.SUCCESS
+}
